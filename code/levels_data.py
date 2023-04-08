0501f4cbc7f3c276fed4024a4943dcc6ab3c6f88
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-04-08 16:31:31 +0200

    new level

--- code/levels_data.py
+++ code/levels_data.py
@@ -5,4 +5,11 @@ level_1 = {
     'coin': 'levels/1/level_1._coins.csv'
 }
 
-levels = [level_1]
+level_2 = {
+    'terrain': 'levels/2/level_2._terrain.csv',
+    'block': 'levels/2/level_2._blocks.csv',
+    'q_block': 'levels/2/level_2._question blocks.csv',
+    'coin': 'levels/2/level_2._coins.csv'
+}
+
+levels = [level_1,level_2]
