7b8511d0923dde5b06537d156f24c7a5fa092d91
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-04-09 12:00:53 +0200

    different starcoin

--- code/levels_data.py
+++ code/levels_data.py
@@ -1,15 +1,15 @@
 level_1 = {
-    'terrain': 'levels/1/level_1._terrain.csv',
-    'block': 'levels/1/level_1._blocks.csv',
-    'q_block': 'levels/1/level_1._question blocks.csv',
-    'coin': 'levels/1/level_1._coins.csv'
+    'terrain': 'levels/1/level_1_terrain.csv',
+    'block': 'levels/1/level_1_blocks.csv',
+    'q_block': 'levels/1/level_1_question blocks.csv',
+    'coin': 'levels/1/level_1_coins.csv'
 }
 
 level_2 = {
-    'terrain': 'levels/2/level_2._terrain.csv',
-    'block': 'levels/2/level_2._blocks.csv',
-    'q_block': 'levels/2/level_2._question blocks.csv',
-    'coin': 'levels/2/level_2._coins.csv'
+    'terrain': 'levels/2/level_2_terrain.csv',
+    'block': 'levels/2/level_2_blocks.csv',
+    'q_block': 'levels/2/level_2_question blocks.csv',
+    'coin': 'levels/2/level_2_coins.csv'
 }
 
 levels = [level_1,level_2]
--- levels/tilesets/coin.tsx
+++ levels/tilesets/coin.tsx
@@ -1,10 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="coin" tilewidth="96" tileheight="96" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="coin" tilewidth="96" tileheight="96" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="48" height="48" source="../../graphics/coin/coin.png"/>
  </tile>
  <tile id="1">
-  <image width="96" height="96" source="../../graphics/starcoin/starcoin.png"/>
+  <image width="96" height="96" source="../../graphics/starcoin/starcoin_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="96" height="96" source="../../graphics/starcoin/starcoin_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="96" height="96" source="../../graphics/starcoin/starcoin_3.png"/>
  </tile>
 </tileset>
