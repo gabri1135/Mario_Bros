4c658c83119faf2eb7cd3a7c8e3c01c8cbd584e1
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-04-19 14:16:06 +0200

    change level id

--- code/levels_data.py
+++ code/levels_data.py
@@ -1,8 +1,21 @@
+level_0 = {
+    'terrain': 'levels/0/level_0_terrain.csv',
+    'block': 'levels/0/level_0_blocks.csv',
+    'q_block': 'levels/0/level_0_question_blocks.csv',
+    'coin': 'levels/0/level_0_coins.csv',
+    'goomba': 'levels/0/level_0_goomba.csv',
+    'goomba_constraints': 'levels/0/level_0_goomba_constraints.csv',
+    'player_data': 'levels/0/level_0_player_data.csv'
+}
+
 level_1 = {
     'terrain': 'levels/1/level_1_terrain.csv',
     'block': 'levels/1/level_1_blocks.csv',
     'q_block': 'levels/1/level_1_question_blocks.csv',
-    'coin': 'levels/1/level_1_coins.csv'
+    'coin': 'levels/1/level_1_coins.csv',
+    'goomba': 'levels/1/level_1_goomba.csv',
+    'goomba_constraints': 'levels/1/level_1_goomba_constraints.csv',
+    'player_data': 'levels/1/level_1_player_data.csv'
 }
 
 level_2 = {
@@ -10,17 +23,9 @@ level_2 = {
     'block': 'levels/2/level_2_blocks.csv',
     'q_block': 'levels/2/level_2_question_blocks.csv',
     'coin': 'levels/2/level_2_coins.csv',
-    'goomba':'levels/2/level_2_goomba.csv',
-    'goomba_constraints':'levels/2/level_2_goomba_constraints.csv'
+    'goomba': 'levels/2/level_2_goomba.csv',
+    'goomba_constraints': 'levels/2/level_2_goomba_constraints.csv',
+    'player_data': 'levels/2/level_2_player_data.csv'
 }
 
-level_3 = {
-    'terrain': 'levels/3/level_3_terrain.csv',
-    'block': 'levels/3/level_3_blocks.csv',
-    'q_block': 'levels/3/level_3_question_blocks.csv',
-    'coin': 'levels/3/level_3_coins.csv',
-    'goomba':'levels/3/level_3_goomba.csv',
-    'goomba_constraints':'levels/3/level_3_goomba_constraints.csv'
-}
-
-levels = [level_1, level_2, level_3]
+levels = [level_0, level_1, level_2]
