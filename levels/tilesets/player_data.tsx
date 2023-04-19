ef817dba04bed2dffd97fbbab1b087c0cf666de4
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-04-19 14:15:06 +0200

    change level id

---
+++ levels/tilesets/player_data.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="mario" tilewidth="80" tileheight="428" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="80" height="428" source="../../graphics/flagpole/pole.png"/>
+ </tile>
+</tileset>
