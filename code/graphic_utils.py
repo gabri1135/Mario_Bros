336c2f5773fe63d8fc9158cb22b9ceef3c4b5cd1
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-04-20 18:15:48 +0200

    outlined_text

---
+++ code/graphic_utils.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+def outlined_surface(surface, pos, display: pygame.Surface):
+    mask = pygame.mask.from_surface(surface)
+    mask_surf = mask.to_surface(setcolor=(0,0,0),unsetcolor=(255,255,255))
+    mask_surf.set_colorkey((255,255,255))
+    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0]-1, pos[1])))
+    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0]+1, pos[1])))
+    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0], pos[1]-1)))
+    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0], pos[1]+1)))
