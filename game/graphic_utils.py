6497198981243c3a60df2737f9b969abf9572156
Gabriele Martini <martinigabriele.gm@gmail.com>
2023-05-01 20:21:10 +0200

    better outlined_text

--- game/graphic_utils.py
+++ game/graphic_utils.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import pygame
 
 
@@ -5,10 +7,16 @@ def outlined_surface(surface, pos, size, display: pygame.Surface):
     mask = pygame.mask.from_surface(surface)
     mask_surf = mask.to_surface(
         setcolor='black', unsetcolor=(255, 255, 255, 0))
-    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0]-size, pos[1])))
-    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0]+size, pos[1])))
-    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0], pos[1]-size)))
-    display.blit(mask_surf, mask_surf.get_rect(center=(pos[0], pos[1]+size)))
+    for a in range(size+1):
+        b = int(sqrt(size**2-a**2))
+        display.blit(mask_surf, mask_surf.get_rect(
+            center=(pos[0]+a, pos[1]+b)))
+        display.blit(mask_surf, mask_surf.get_rect(
+            center=(pos[0]-a, pos[1]+b)))
+        display.blit(mask_surf, mask_surf.get_rect(
+            center=(pos[0]+a, pos[1]-b)))
+        display.blit(mask_surf, mask_surf.get_rect(
+            center=(pos[0]-a, pos[1]-b)))
 
 
 def outlined_text(text,  size=25, color='white', **kwargs):
@@ -20,6 +28,6 @@ def outlined_text(text,  size=25, color='white', **kwargs):
         (text_surf.get_width()+10, text_surf.get_height()+10), pygame.SRCALPHA)
 
     outlined_surface(text_surf, text_rect.center,
-                     1 if size <= 25 else 2, surface)
+                     int((size-17.5)//7.5), surface)
     surface.blit(text_surf, text_rect)
     return surface, surface.get_rect(**kwargs)
